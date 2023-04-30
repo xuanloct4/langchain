87360192f8248b93a44c05bee20e822d232bbfcc
loctv <xuanloctn@gmail.com>
2023-04-30 22:25:29 +0700

    first commit

---
+++ baby_agi.py
@@ -0,0 +1,43 @@
+
+
+import environment
+
+import os
+from collections import deque
+from typing import Dict, List, Optional, Any
+
+from langchain import LLMChain, OpenAI, PromptTemplate
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.llms import BaseLLM
+from langchain.vectorstores.base import VectorStore
+from pydantic import BaseModel, Field
+from langchain.chains.base import Chain
+from langchain.experimental import BabyAGI
+
+from langchain.vectorstores import FAISS
+from langchain.docstore import InMemoryDocstore
+
+import faiss
+
+# Define your embedding model
+embeddings_model = OpenAIEmbeddings()
+# Initialize the vectorstore as empty
+
+
+embedding_size = 1536
+index = faiss.IndexFlatL2(embedding_size)
+vectorstore = FAISS(embeddings_model.embed_query, index, InMemoryDocstore({}), {})
+
+OBJECTIVE = "Write a weather report for SF today"
+
+llm = OpenAI(temperature=0)
+
+# Logging of LLMChains
+verbose = False
+# If None, will keep on going forever
+max_iterations: Optional[int] = 3
+baby_agi = BabyAGI.from_llm(
+    llm=llm, vectorstore=vectorstore, verbose=verbose, max_iterations=max_iterations
+)
+
+baby_agi({"objective": OBJECTIVE})
---
+++ environment.py
@@ -0,0 +1,25 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv() 
+
+
+os.environ["LANGCHAIN_HANDLER"] = os.environ.get("LANGCHAIN_HANDLER")
+
+## Uncomment this if using hosted setup.
+
+# os.environ["LANGCHAIN_ENDPOINT"] = "http://localhost:4173" 
+
+## Uncomment this if you want traces to be recorded to "my_session" instead of default.
+
+os.environ["LANGCHAIN_SESSION"] = os.environ.get("LANGCHAIN_SESSION")
+
+## Better to set this environment variable in the terminal
+## Uncomment this if using hosted version. Replace "my_api_key" with your actual API Key.
+
+# os.environ["LANGCHAIN_API_KEY"] = "my_api_key"  
+
+
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
